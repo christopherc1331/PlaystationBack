727fadb737568cf9b817246614f2e7a6ea4767dd
Christopher Cruz <christopherc1331@gmail.com>
2021-10-06 19:41:41 -0700

    Initialized repo

---
+++ src/main/java/playstation/back/featureflags/PlaystationBackApplication.java
@@ -0,0 +1,13 @@
+package playstation.back.featureflags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaystationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaystationBackApplication.class, args);
+    }
+
+}
